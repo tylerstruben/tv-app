b06367eabf3e8152bda4af40596b055b6daed771
tylerstruben <tylerstruben@gmail.com>
2023-12-10 16:43:42 -0500

    tv-app

--- src/tv-app.js
+++ src/tv-app.js
@@ -1,115 +1,187 @@
-// import stuff
 import { LitElement, html, css } from 'lit';
 import '@shoelace-style/shoelace/dist/components/dialog/dialog.js';
 import '@shoelace-style/shoelace/dist/components/button/button.js';
 import "./tv-channel.js";
-
+import "@lrnwebcomponents/video-player/video-player.js";
 export class TvApp extends LitElement {
-  // defaults
-  constructor() {
-    super();
-    this.name = '';
-    this.source = new URL('../assets/channels.json', import.meta.url).href;
-    this.listings = [];
-    this.activeItem = {
-      title: null,
-      id: null,
-      description: null,
-    };
-  }
-  // convention I enjoy using to define the tag's name
   static get tag() {
     return 'tv-app';
   }
-  // LitElement convention so we update render() when values change
   static get properties() {
     return {
-      name: { type: String },
+      appName: { type: String },
       source: { type: String },
-      listings: { type: Array },
-      activeItem: { type: Object }
+      channelList: { type: Array },
+      activeIndex: { type: Number}
+      
     };
   }
-  // LitElement convention for applying styles JUST to our element
+  constructor() {
+    super();
+    this.appName = '';
+    this.source = new URL('../assets/channels.json', import.meta.url).href;
+    this.channelList = [];
+    this.activeIndex = 0;
+  }
   static get styles() {
     return [
       css`
-      :host {
-        display: block;
-        margin: 16px;
-        padding: 16px;
-      }
-      `
+        :host {
+          font-family: 'Arial', sans-serif;
+          display: block;
+          background-color: #f8f8f8;
+          color: #333;
+        }
+        .app-container {
+          display: grid;
+          grid-template-columns: 2fr 1fr 1fr;
+          gap: 20px;
+          padding: 20px;
+        }
+        .video-section {
+          grid-column: 1;
+        }
+        .description-box {
+          background-color: #fff;
+          border-radius: 8px;
+          box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+          padding: 20px;
+          margin-top: 20px;
+        }
+        .channel-list {
+          grid-column: 2;
+          background-color: #fff;
+          padding: 20px;
+          border-radius: 10px;
+          box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1); 
+          overflow-y: auto; 
+          max-height: 700px;
+        }
+        .thumbnail {
+          max-width: 100%;
+          height: auto;
+        }
+        .controls {
+          display: flex;
+          justify-content: space-between;
+          margin-top: 20px;
+          padding: 10px 20px;
+          
+          border-radius: 5px;
+        
+        }
+        .prev-button,
+        .next-button {
+          font-size: 16px;
+          padding: 10px 20px;
+          background-color: #007bff;
+          color: #fff;
+          border: none;
+          border-radius: 5px;
+          cursor: pointer;
+        }
+        .prev-button:hover,
+        .next-button:hover {
+          background-color: #0056b3;
+        }
+      `,
     ];
   }
-  // LitElement rendering template of your element
   render() {
     return html`
-      <h2>${this.name}</h2>
-      ${
-        this.listings.map(
-          (item) => html`
-            <tv-channel
-              id="${item.id}"
-              title="${item.title}"
-              presenter="${item.metadata.author}"
-              description="${item.description}"
-              @click="${this.itemClick}"
-            >
-            </tv-channel>
-          `
-        )
-      }
-      <div>
-      ${this.activeItem.name}
-      ${this.activeItem.description}
-        <!-- video -->
-        <!-- discord / chat - optional -->
+      <div class="app-container">
+        <div class="video-section">
+          
+          
+      <video-player source="https://www.youtube.com/watch?v=hrbNqEFTbrU" accent-color="blue"></video-player> 
+      <h2>Pink Panther. My Favorite Episode.</h2> 
+      <div class="description-box">
+  
+      ${this.channelList.length > 0 ? this.channelList[this.activeIndex].description : ''}
+
       </div>
-      <!-- dialog -->
-      <sl-dialog label="Dialog" class="dialog">
-      ${this.activeItem.title}
-        <sl-button slot="footer" variant="primary" @click="${this.closeDialog}">Close</sl-button>
-      </sl-dialog>
+      <div class="controls">
+      <button class="prev-button" @click="${this.prevSlide}">Previous</button>
+      <button class="next-button" @click="${this.nextSlide}">Next</button>
+      </div>
+        </div>  
+        <div class="channel-list">
+          <h2>${this.appName}</h2>
+          ${this.channelList.map(
+            (item, index) => html`
+              <tv-channel 
+                ?active="${index === this.activeIndex}"
+                index="${index}"
+                title="${item.title}"
+                presenter="${item.metadata.author}"
+                @click="${this.itemClick}"
+                timecode="${item.metadata.timecode}"
+                thumbnail="${item.metadata.thumbnail}"
+                minuteTranslation="${item.metadata.minuteTranslation}" 
+                
+              ></tv-channel> 
+              
+            `
+          )}
+          
+        </div>
+      </div>
+      
     `;
   }
-
-  closeDialog(e) {
-    const dialog = this.shadowRoot.querySelector('.dialog');
-    dialog.hide();
-  }
-
-  itemClick(e) {
+  itemClick(e) { 
+    
+    // Handle channel item click
     console.log(e.target);
-    this.activeItem = {
-      title: e.target.title,
-      id: e.target.id,
-      description: e.target.description,
-    };
-    const dialog = this.shadowRoot.querySelector('.dialog');
-    dialog.show();
-  }
-
-  // LitElement life cycle for when any property changes
-  updated(changedProperties) {
-    if (super.updated) {
-      super.updated(changedProperties);
+    this.activeIndex= e.target.index; 
+    this.shadowRoot.querySelector('video-player').shadowRoot.querySelector("a11y-media-player").media.currentTime;
+    this.shadowRoot.querySelector('video-player').shadowRoot.querySelector('a11y-media-player').play();
+    this.shadowRoot.querySelector('video-player').shadowRoot.querySelector('a11y-media-player').seek(e.target.timecode);
     }
+
+  updated(changedProperties) {
+    super.updated(changedProperties);
     changedProperties.forEach((oldValue, propName) => {
       if (propName === "source" && this[propName]) {
-        this.updateSourceData(this[propName]);
+        this.updateChannelList(this[propName]);
       }
+
+      if(propName === "activeIndex"){ 
+        console.log(this.shadowRoot.querySelectorAll("tv-channel"));
+        console.log(this.activeIndex)
+
+        var activeChannel = this.shadowRoot.querySelector("tv-channel[index = '" + this.activeIndex + "' ] ");
+
+        console.log(activeChannel);
+        this.shadowRoot.querySelector('video-player').shadowRoot.querySelector('a11y-media-player').seek(activeChannel.timecode);
+      }
+
     });
   }
-
-  async updateSourceData(source) {
+  prevSlide() {
+    this.activeIndex = Math.max(0, this.activeIndex - 1);
+    
+  }
+  nextSlide() {
+    this.activeIndex = Math.min(this.channelList.length - 1, this.activeIndex + 1);  
+  }
+  connectedCallback() {
+    super.connectedCallback();
+    
+    setInterval(() => {
+      const currentTime = this.shadowRoot.querySelector('video-player').shadowRoot.querySelector('a11y-media-player').media.currentTime;
+      if (this.activeIndex + 1 < this.channelList.length &&
+          currentTime >= this.channelList[this.activeIndex + 1].metadata.timecode) {
+        this.activeIndex++;
+      }
+    }, 1000);
+  }
+  async updateChannelList(source) {
     await fetch(source).then((resp) => resp.ok ? resp.json() : []).then((responseData) => {
       if (responseData.status === 200 && responseData.data.items && responseData.data.items.length > 0) {
-        this.listings = [...responseData.data.items];
-        console.log(this.listings);
+        this.channelList = [...responseData.data.items];
       }
     });
   }
 }
-// tell the browser about our tag and class it should run when it sees it
-customElements.define(TvApp.tag, TvApp);
+customElements.define(TvApp.tag, TvApp); 
--- src/tv-channel.js
+++ src/tv-channel.js
@@ -1,46 +1,83 @@
-// import stuff
 import { LitElement, html, css } from 'lit';
 
 export class TvChannel extends LitElement {
-  // defaults
-  constructor() {
-    super();
-    this.title = '';
-    this.presenter = '';
-  }
-  // convention I enjoy using to define the tag's name
   static get tag() {
     return 'tv-channel';
   }
-  // LitElement convention so we update render() when values change
+
   static get properties() {
     return {
       title: { type: String },
+      video: { type: String },
       presenter: { type: String },
+      timecode: { type: Number },
+      description: { type: String },
+      thumbnail: { type: String },
     };
   }
-  // LitElement convention for applying styles JUST to our element
+
+  constructor() {
+    super();
+    this.title = '';
+    this.video = '';
+    this.presenter = '';
+    this.thumbnail = '';
+  }
+
   static get styles() {
     return css`
       :host {
-        display: inline-flex;
-      }
-      .wrapper {
-        padding: 16px;
+        display: block;
+        padding: 15px;
         background-color: #eeeeee;
+        margin-top: 12px;
+        border-radius: 8px;
+        transition: background-color 0.3s ease;
+      }
+
+      :host([active]) {
+        background-color: #007bff;
+      }
+
+      .thumbnail {
+        max-width: 100%;
+        height: auto;
+        margin-bottom: 10px;
+        object-fit: cover;
+        
+      }
+
+      h3, h4, h5 {
+        margin: 0;
+        line-height: 1.2;
+      }
+
+      h3 {
+        font-size: 18px;
+        color: #333;
+      }
+
+      h4 {
+        font-size: 16px;
+        color: #555;
+      }
+
+      h5 {
+        font-size: 14px;
+        color: #777;
       }
     `;
   }
-  // LitElement rendering template of your element
+
   render() {
     return html`
-      <div class="wrapper">
-        <h3>${this.title}</h3>
-        <h4>${this.presenter}</h4>
-        <slot></slot>
-      </div>  
-      `;
+      <img class="thumbnail" src="${this.thumbnail}" alt="${this.title}">
+      <h3>${this.title}</h3>
+      <h4>${this.presenter}</h4>
+      <h5>${this.description}</h5>
+      <slot></slot>
+    `;
   }
 }
-// tell the browser about our tag and class it should run when it sees it
+
 customElements.define(TvChannel.tag, TvChannel);
